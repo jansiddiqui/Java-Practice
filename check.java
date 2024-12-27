6f66e74a4286a2d040f74781d832bfb40f6eedad
Jan <jan810693@gmail.com>
2024-12-27 10:06:27 +0530

    update

--- check.java
+++ check.java
@@ -1,5 +1,7 @@
 public class check {
     public static void main(String[] args){
-        System.out.print("Hello Jan Mohammad. Here is Narendra Damodar Das Modi... i want to give you internship in my own website you have develope it efficiently as soon as possible for this I will pay you 500000.... I hope you will accept my offer... Thank you so for time and attention I will always be payfull for your your kind attention....");
+        System.out.println("Hello Jan Mohammad. Here is Narendra Damodar Das Modi... i want to give you internship in my own website you have develope it efficiently as soon as possible for this I will pay you 500000.... I hope you will accept my offer... Thank you so for time and attention I will always be payfull for your your kind attention....");
+        System.out.print("Hello world java\nBy Jan Mohammad Siddiqui\n");
+        System.out.print("Hello world java");
     }
 }
