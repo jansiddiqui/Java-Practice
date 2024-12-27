64498ec6f33db2a96bb9d400d50d92437b2eed6b
Jan <jan810693@gmail.com>
2024-12-27 09:55:11 +0530

    check

---
+++ check.java
@@ -0,0 +1,5 @@
+public class check {
+    public static void main(String[] args){
+        System.out.print("Hello Jan Mohammad. Here is Narendra Damodar Das Modi... i want to give you internship in my own website you have develope it efficiently as soon as possible for this I will pay you 500000.... I hope you will accept my offer... Thank you so for time and attention I will always be payfull for your your kind attention....");
+    }
+}
