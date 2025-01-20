01ad35a66dabe0c45c8e476ac1cde4bfeb0a0ba1
Jan <jan810693@gmail.com>
2025-01-20 11:39:55 +0530

    java journey started

---
+++ Jan.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Jan{
+    public static void main(String[] args){
+        Scanner obj = new Scanner(System.in);
+        int a = obj.nextInt();
+        short b = obj.nextShort();
+        byte c = obj.nextByte();
+        String d = obj.next();
+        String e  = obj.nextLine();
+        System.out.println("Name is "+a+" short is "+b+" Byte is "+c+" String is "+d+" line is "+e);
+    }
+}
---
+++ Variables.java
@@ -0,0 +1,10 @@
+public class Variables {
+    public static void main(String[] args) {
+        //variable
+        String name = "Himanshu";
+        int age = 40;
+        int height = 5;
+        double weight = 70.5;
+        
+    }
+}
