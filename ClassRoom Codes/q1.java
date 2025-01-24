d05cc049d37e263fe845f47b03aaa8e8cdbd7780
Jan <jan810693@gmail.com>
2025-01-24 11:56:04 +0530

    question one

---
+++ ClassRoom Codes/q1.java
@@ -0,0 +1,7 @@
+public class q1 {
+        public static void main(String[] args) { 
+            int value = 21, sum = 5 + -- value;
+            int data = --value + ++value / sum++ * value++ + ++sum  % value--;
+            System.out.println(data);
+        } 
+}
