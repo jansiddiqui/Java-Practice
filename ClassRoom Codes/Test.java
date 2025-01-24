08ef0ba553abfe86f357a06dd1c285a7de0c6f48
Jan <jan810693@gmail.com>
2025-01-24 11:35:26 +0530

    ternary operator

---
+++ ClassRoom Codes/Test.java
@@ -0,0 +1,10 @@
+class Test {
+    public static void main(String args[])
+      { 
+          int var1 = 5; 
+          int var2 = 6; 
+          int var3; 
+          var3 = ++var2 * var1 / var2 + var2;             			
+          System.out.print(var3);
+      }
+  }
---
+++ ClassRoom Codes/ternary.java
@@ -0,0 +1,10 @@
+public class ternary {
+    public static void main(String[] args){
+        // int a=10,b=20,c;
+        // c=(a>b)?a:b;
+        int a=91;
+        char c=(a>90)?'A':(a>80)?'B':(a>70)?'C':'D';
+        System.out.println("Grade of A is : " +c);
+    }
+}
+
