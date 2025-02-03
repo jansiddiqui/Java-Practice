686e7e5b5bb0c08c83078f426e6b63d106dc43ea
Jan <jan810693@gmail.com>
2025-02-03 10:51:33 +0530

    jan

--- ClassRoom Codes/multi.java
+++ ClassRoom Codes/multi.java
@@ -42,14 +42,77 @@
 // }
 
 
-class multi {
-	public static void main(String[] args) {
-		int k = 0;
-        for (int i = 10; i > 0; i--) { 
-            while (i > 3) 
-                i -= 3; 
-                k += 1;
-            }
-        System.out.println(k); 
-	}
+// class multi {
+// 	public static void main(String[] args) {
+// 		int k = 0;
+//         for (int i = 10; i > 0; i--) { 
+//             while (i > 3) 
+//                 i -= 3; 
+//                 k += 1;
+//             }
+//         System.out.println(k); 
+// 	}
+// }
+
+// class multi{
+//     public static void main(String[] args){
+//         int i=0;
+//         for(System.out.println("Bye "); i<10; i++){
+//             System.out.println("Hello");
+//         }
+//     }
+// }
+
+// class multi{
+//     public static void main(String[] args){
+//         int i=0;
+//         for(System.out.println("Bye "); i<10; i++, System.out.println("class ")){
+//             System.out.println("Hello");
+//         }
+//     }
+// }
+
+// class multi{
+//     public static void main(String[] args){
+        
+//         for(; ; ){
+//             System.out.println("Hello");
+//         }
+//     }
+// }
+
+// class multi{
+//     public static void main(String[] args){
+//         int i =0;
+//         for(char a='A'; i<'E'; i++){
+//             System.out.println(a);
+//         }
+//     }
+// }
+
+// class multi{
+//     public static void main(String[] args){
+//         boolean i=true;
+//         for(boolean b=false; i; i=true){
+//             System.out.println(b);
+//         }
+//     }
+// }
+
+// class multi{
+//     public static void main(String[] args){
+//         int a[] = {10, 20, 60, 40, 59};
+//         for(int s : a){
+//             System.out.println(s);
+//         }
+//     }
+// }
+
+
+class multi{
+    public static void main(String[] args){
+        for(char ch : "BNGHJJKM".toCharArray()){
+            System.out.println(length.ch[]);
+        }
+    }
 }
