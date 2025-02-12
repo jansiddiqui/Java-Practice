fe7a0c844b50711a74b39ba31fd10d1c4eceb555
Jan <jan810693@gmail.com>
2025-02-12 10:23:39 +0530

    declaration of one d array

---
+++ Java Neo/Array/OneD.java
@@ -0,0 +1,8 @@
+class OneD {
+    public static void main(String[] args){
+        int a[] = {56, 34, 32, 67};
+        for(int i=0;i<a.length;i++){
+            System.out.print(a[i] + " ");
+        }
+    }
+}
