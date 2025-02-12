6216f371b98cf6acd257ef8d50856c4066c4ff32
Jan <jan810693@gmail.com>
2025-02-12 10:20:16 +0530

    simple array initialization

---
+++ Java Neo/Array/Test.java
@@ -0,0 +1,31 @@
+// class Test {
+//     public static void main(String args[]){
+//         int a[] = new int[4];
+//         a[0] = 10;
+//         a[1] = 11;
+//         a[2] = 12;
+//         a[3] = 13;
+//         for(int i=0;i<a.length;i++){
+//             System.out.print(a[i] + " ");
+//         }
+//     }
+// }
+
+class Test{
+    public static void main(String[] args){
+        int a[] = new int[10];
+        a[0] = 12;
+        a[1] =123;
+        a[2]= 1234;
+        a[3] = 32;
+        a[4] = 54;
+        a[5] =65;
+        a[6] = 5432;
+        a[7] = 879;
+        a[8] = 789;
+        a[9] = 567;
+        for(int i=0; i<=a.length; i++){
+            System.out.println(a[i]);
+        }
+    }
+}
