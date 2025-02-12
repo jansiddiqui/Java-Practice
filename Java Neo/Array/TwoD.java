49718f702d687360006a58dbe7f59021300ab57b
Jan <jan810693@gmail.com>
2025-02-12 10:28:16 +0530

    2d array declaration

---
+++ Java Neo/Array/TwoD.java
@@ -0,0 +1,7 @@
+class TwoD {
+    public static void main(String[] args){
+        int arr[][] = new int[10][10];
+        arr[0][0] = 1;
+        System.out.println("arr[0][0] =" + arr[0][0]);
+    }
+}
