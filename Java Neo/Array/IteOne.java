e2746132245b62bdd49023a50d76510540dee5f9
Jan <jan810693@gmail.com>
2025-02-12 11:14:04 +0530

    Iterating over an 1D array using for each loop

---
+++ Java Neo/Array/IteOne.java
@@ -0,0 +1,9 @@
+
+public class IteOne {
+    public static void main(String[] args){
+        int arr[] = {10,20,30,40,50};
+        for(int element : arr){
+            System.out.println(element + " ");
+        }
+    }
+}
