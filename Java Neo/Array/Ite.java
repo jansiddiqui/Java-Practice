6a07cfe55eddec6166595b02687279db78f6e6b1
Jan <jan810693@gmail.com>
2025-02-12 10:56:03 +0530

    array element iteration

---
+++ Java Neo/Array/Ite.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+class Ite{
+    public static void main(String[] args){
+        int myArray[] = new int[7];
+        myArray[0] = 134;
+        myArray[1] = 5367;
+        myArray[2] = 6787;
+        myArray[3] = 123;;
+        myArray[4] = 4567;
+        myArray[5] = 1284;
+        myArray[6] = 1984;
+        System.out.println("Element at 5th index is " + myArray[5]);
+    }
+}
