9fc04d3c3b2d455e749abdd91841be611574dc14
Jan <jan810693@gmail.com>
2025-02-14 10:53:21 +0530

    enum

--- ClassRoom Codes/enum1.java
+++ ClassRoom Codes/enum1.java
@@ -1,11 +1,34 @@
 enum Day{
-    Mon, Tue, Thu, Wed, Fri, Sat, Sun
+    Mon, Tue, Thu, Wed, Fri, Sat
 }
 public class enum1 {
     public static void main(String[] args){
         for(Day s:Day.values())
-        System.out.println(s+ " "+ (Day.valueOf(s.toString()).ordinal())); 
-        // ordinal used for accessing the position of the perticular string i  enum
+        // System.out.println(s+ " "+ (Day.valueOf(s.toString()).ordinal())); 
+        // ordinal used for accessing the position of the perticular string in enum
+        switch(s){
+            case Mon:
+                System.out.println("Monday");
+                break;
+            case Tue:
+                System.out.println("Tuesday");
+                break;
+            case Thu:
+                System.out.println("Thursday");
+                break;
+            case Wed:
+                System.out.println("Wednesday");
+                break;
+            case Fri:
+                System.out.println("Friday");
+                break;
+            case Sat:
+                System.out.println("Saturday");
+                break;
+            default:
+                System.out.println("Invalid");
+        }
     }
-    
 }
+
+// if we called the switch case more than number of elements in enum then default will not execute
