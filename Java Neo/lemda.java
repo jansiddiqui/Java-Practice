312d6f7eaa317d8bfe8185bce95dfbed013f36eb
Jan <jan810693@gmail.com>
2025-03-21 11:51:55 +0530

    lemda expression

--- Java Neo/lemda.java
+++ Java Neo/lemda.java
@@ -1,14 +1,38 @@
+// @FunctionalInterface
+// interface F{
+//     void sq(int a);
+// }
+
 @FunctionalInterface
 interface F{
-    void sq(int a);
+    int sq(int a);
 }
-class lemda implements F{
-    @Override
-    public void sq(int a){
-        System.out.println("Square of "+ a + " is: "+a*a);
+// class lemda implements F{
+//     @Override
+//     public void sq(int a){
+//         System.out.println("Square of "+ a + " is: "+a*a);
+//     }
+//     public static void main(String a[]){
+//         F f = new lemda();
+//         f.sq(5);
+//     } 
+// }
+
+// class lemda{
+//     public static void main(String[] args) {
+//         F obj = a ->{
+//             System.out.println("Square is: "+ a*a);
+//         };
+//         obj.sq(5);
+//     }
+// }
+
+class lemda{
+    public static void main(String[] args) {
+        F obj = a ->{
+            System.out.println("Square is: "+ a*a);
+            return 0;
+        };
+        obj.sq(5);
     }
-    public static void main(String a[]){
-        F f = new lemda();
-        f.sq(5);
-    } 
 }
